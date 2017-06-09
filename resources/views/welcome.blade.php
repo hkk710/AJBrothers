de924698b7765e0cc793257cf939234570ec0dad
hkk710 <hkk710@gmail.com>
2017-06-10 00:05:36 +0530

    bug fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
 		<div class="row">
 			<div class="col-md-12 col-sm-12">
 				<h3>WEB DESIGN / WEB DEVELOPMENT / SOCIAL MEDIA</h3>
-				<h1 style="color:#00c6d7">AJ Brothres</h1>
+				<h1 style="color:#00c6d7">AJ Brothers</h1>
 				<hr>
 				<a href="#work" class="smoothScroll btn btn-danger">What we do</a>
 				<a href="#contact" class="smoothScroll btn btn-default">Talk to us</a>
@@ -199,9 +199,9 @@
 							<h3>Core Developer</h3>
 							<hr>
 							<ul class="social-icon">
-								<li><a href="#" class="fa fa-facebook wow fadeIn" data-wow-delay="0.3s"></a></li>
-								<li><a href="#" class="fa fa-twitter wow fadeIn" data-wow-delay="0.6s"></a></li>
-								<li><a href="#" class="fa fa-google-plus wow fadeIn" data-wow-delay="0.9s"></a></li>
+								<li><a href="http://facebook.com/advaith3600" class="fa fa-facebook wow fadeIn" data-wow-delay="0.3s"></a></li>
+								<li><a href="http://twitter.com/advaith3600" class="fa fa-twitter wow fadeIn" data-wow-delay="0.6s"></a></li>
+								<li><a href="https://plus.google.com/+AdvaithAJ" class="fa fa-google-plus wow fadeIn" data-wow-delay="0.9s"></a></li>
 							</ul>
 						</div>
 				</div>
